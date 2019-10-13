7a3ea44539ad85e39e23102f93382a3d60331c2a
adt6 <aditya.thakur5753235@gmail.com>
2019-10-13 11:23:13 +0400

    Created a class named Entity and Bullet.The entity class is parent class for both player.class and bullet.class .i have ccreated a shooting function in the player.class and implemented update function in bullet.class .

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -2,6 +2,8 @@ import {Player} from './model/Player';
 import * as playerView from './views/playerView';
 import * as base from './views/base';
 import {getControl} from './Control';
+import {Bullet} from './model/Bullet';
+
 
 
 
@@ -22,7 +24,12 @@ const Update = () =>{
     const controls = getControl(player);
 
     // updates movement and the angle
-    player.update(controls.movement, controls.angle);
+    player.update(controls.movement, controls.angle, controls.click);
+    
+    for(const i in Bullet.list){
+        const bullet = Bullet.list[i];
+        bullet.update();
+    }
 
     //draws the player in the canvas
     playerView.drawPlayer(player, base.elements.ctxMain);
