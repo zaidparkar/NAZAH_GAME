6cf9399e6c361b7f13b41f8334cd17d0ead6dbd6
Zaid Parkar <parkar139@gmail.com>
2019-10-18 20:11:13 +0400

    Added two functions which calculate distance and determine whether tje two objects collide

--- src/client/js/model/Entity.js
+++ src/client/js/model/Entity.js
@@ -8,4 +8,17 @@ export class Entity{
        this.angle=0;
 
     }
+    //gets distance between the two objects 
+    getDistance(obj1,obj2)  {
+        var distX = obj2.x - obj1.x;
+        var distY = obj2.y - obj1.y;
+        return Math.sqrt(Math.pow(distX,2)+Math.pow(distY,2));
+    }
+
+    //Alerts the console that the objects have collided
+    checkCollision(obj1,obj2)  {
+        if(getDistance(obj1,obj2) < 10);
+            console.log("Collision Alert");
+    }
+  
 }
