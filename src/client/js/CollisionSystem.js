9e0b9139a5e43e865cceb9be93df6952eabb6f38
Mohammed Ashab Uddin <mu15@hw.ac.uk>
2019-10-20 20:36:38 +0400

    Added CollisionSystem.js with the variable for the map

---
+++ src/client/js/CollisionSystem.js
@@ -0,0 +1,29 @@
+
+
+const grid = [];
+
+
+const mapSize = {
+    x: 500,
+    y: 500
+};
+
+const cellSize = {
+    x: 25,
+    y: 25
+}
+
+
+//Cell class for the cells in the grid
+class Cell{
+
+    constructor(occupied, id){
+        this.occupied = occupied;
+        this.id = id;
+    }
+}
+
+
+
+
+
