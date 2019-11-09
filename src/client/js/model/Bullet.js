906030617b075daf86abd8e947a9f6791fa9c247
Mohammed Ashab Uddin <mu15@hw.ac.uk>
2019-11-09 09:19:06 +0400

    Bullet will delete itself after a ceertain time

--- src/client/js/model/Bullet.js
+++ src/client/js/model/Bullet.js
@@ -5,19 +5,25 @@ export class Bullet extends Entity{
     constructor(id,x=250,y=250){
       super(id, x, y);
       Bullet.list[this.id] = this;
+      this.timer = 0;
+      this.speed = 24;
     }
 
     update(){
+      this.timer ++;  
       this.x += Math.cos(this.angle) * this.speed;
       this.y += Math.sin(this.angle) * this.speed ;
-      this.timer=0;
-      this.toRemove=false;
-      if(this.timer++ > 100){
-        this.toRemove=true;
+      console.log(this.timer);
+      if(this.timer > 24){
+        delete Bullet.list[this.id];
       }
 
     }
     
+    checkOccupied()
+    {
+
+    }
 
   
 }
